83d07b8ec72b936d75ec0adbf046dc04dca5a65f
soumadip-dev <soumadipmajila@gmail.com>
2024-12-02 12:00:00 +0530

    feat: add controller to update task

--- Task-Manager/controllers/task.controllers.js
+++ Task-Manager/controllers/task.controllers.js
@@ -1,4 +1,5 @@
 import { readTask, writeTask } from '../utils/task.js';
+import crypto from 'crypto';
 
 // Controller to get all tasks for the logged-in user
 export const getAllTasks = async (req, res) => {
@@ -41,8 +42,46 @@ export const createTask = async (req, res) => {
   });
 };
 
-// TODO: Implement task update logic
-export const updateTask = (req, res) => {};
+// Controller to update an existing task
+export const updateTask = async (req, res) => {
+  // Check if user is authenticated
+  if (!req.session.user) {
+    return res.status(401).json({
+      success: false,
+      error: 'Unauthorized',
+    });
+  }
 
-// TODO: Implement task delete logic
+  const { id } = req.params;
+  const { title, description, complete } = req.body;
+
+  // Read existing tasks
+  const tasks = await readTask();
+
+  // Check if the id is valid and the task belongs to the current user
+  const taskToUpdate = tasks.find(
+    task => task.id === id && task.userName === req.session.user.userName
+  );
+  if (!taskToUpdate) {
+    return res.status(404).json({
+      success: false,
+      error: 'Task not found or does not belong to the current user',
+    });
+  }
+
+  // Update the task with the new values if new value is provided
+  if (title) taskToUpdate.title = title;
+  if (description) taskToUpdate.description = description;
+  if (complete !== undefined) taskToUpdate.complete = complete;
+
+  // Save updated task list
+  await writeTask(tasks);
+
+  res.status(200).json({
+    success: true,
+    message: 'Task updated successfully',
+  });
+};
+
+// Controller to delete a task
 export const deleteTask = (req, res) => {};
